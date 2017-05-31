59c835b41d6f26c142dc06992ce14fc650d8f7cf
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2017-05-31 10:05:49 +0200

    add SDL Project

---
+++ 03-lab/CircleSDL/app/src/main/cpp/my_module/main-circle.cpp
@@ -0,0 +1,72 @@
+/**
+ * Note: this code has been taken from https://github.com/stephen47/android-sdl2-gradle-template
+ */
+
+
+#include "SDL.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "SDL",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        200,                               // width, in pixels
+        200,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+    // Setup renderer
+    SDL_Renderer* renderer = NULL;
+    renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create renderer: %s\n", SDL_GetError());
+        return 1;
+    }
+
+
+    while (1) {
+        // Set render color to red ( background will be rendered in this color )
+        SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255);
+
+        // Clear winow
+        SDL_RenderClear( renderer );
+
+        int width;
+        int height;
+        int radius = 100;
+        SDL_GetWindowSize(window, &width, &height);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        for (int w = 0; w < radius * 2; w++)
+        {
+            for (int h = 0; h < radius * 2; h++)
+            {
+                int dx = radius - w; // horizontal offset
+                int dy = radius - h; // vertical offset
+                if ((dx*dx + dy*dy) <= (radius * radius))
+                {
+                    SDL_RenderDrawPoint(renderer, width/2 + dx, height/2 + dy);
+                }
+            }
+        }
+
+        // Render the rect to the screen
+        SDL_RenderPresent(renderer);
+    }
+}
---
+++ 03-lab/CircleSDL/app/src/main/cpp/my_module/main.cpp
@@ -0,0 +1,75 @@
+/**
+ * Note: this code has been taken from https://github.com/stephen47/android-sdl2-gradle-template
+ */
+
+
+#include "SDL.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "SDL Chess",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+    // Setup renderer
+    SDL_Renderer* renderer = NULL;
+    renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create renderer: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Rect rect, darea;
+    int row = 0,column = 0,x = 0;
+
+    while (1) {
+        // Set render color to red ( background will be rendered in this color )
+        SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255);
+
+        // Clear winow
+        SDL_RenderClear( renderer );
+
+        /* Get the Size of drawing surface */
+        SDL_RenderGetViewport(renderer, &darea);
+        for( ; row < 8; row++)
+        {
+            column = row%2;
+            x = column;
+            for( ; column < 4+(row%2); column++)
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+                rect.w = darea.w/8;
+                rect.h = darea.h/8;
+                rect.x = x * rect.w;
+                rect.y = row * rect.h;
+                x = x + 2;
+
+                SDL_RenderFillRect(renderer, &rect);
+            }
+        }
+
+        // Render the rect to the screen
+        SDL_RenderPresent(renderer);
+    }
+}
