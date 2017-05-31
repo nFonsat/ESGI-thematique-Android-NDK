b3c30b5e3105b5a160f2bb5c82eb4fd933e9d136
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2017-05-31 09:31:29 +0200

    add sample SDL Project

---
+++ 03-lab/GraphicSDL/app/src/main/cpp/my_module/main-mouse.cpp
@@ -0,0 +1,73 @@
+/**
+ * Note: this code has been taken from https://github.com/stephen47/android-sdl2-gradle-template
+ */
+
+
+#include "SDL.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "SDL",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        200,                               // width, in pixels
+        200,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+    // Setup renderer
+    SDL_Renderer* renderer = NULL;
+    renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create renderer: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Point mouse_position;
+
+    while (1) {
+        // Set render color to red ( background will be rendered in this color )
+        SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255);
+
+        // Clear winow
+        SDL_RenderClear( renderer );
+
+        SDL_GetMouseState(                    //    Sets mouse_position to...
+                &mouse_position.x,                // ...mouse arrow coords on window
+                &mouse_position.y
+        );
+
+        SDL_Log("Mouse position: x=%i y=%i",  //    Print mouse position
+                mouse_position.x, mouse_position.y
+        );
+
+        SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255);
+
+        SDL_Rect rect;
+        rect.x = mouse_position.x;
+        rect.y = mouse_position.y;
+        rect.w = 100;
+        rect.h = 100;
+
+        SDL_RenderFillRect(renderer, &rect);
+
+        // Render the rect to the screen
+        SDL_RenderPresent(renderer);
+    }
+}
---
+++ 03-lab/GraphicSDL/app/src/main/java/com/meoagency/android/sdlexample/MainActivity.java
@@ -0,0 +1,6 @@
+package com.meoagency.android.sdlexample;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+}
